6cf8d953344d3a24f521536b9afae89dc1b5e9cf
Jackson Bullard <jacksonbullard@gmail.com>
2024-01-14 20:11:03 -0600

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+
+namespace YetAnotherLightSwitchMod
+{
+	[BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+	public class Plugin : BaseUnityPlugin
+	{
+		public const string PLUGIN_GUID = "com.jacksonb-cs.YetAnotherLightSwitchMod";
+		public const string PLUGIN_NAME = "YetAnotherLightSwitchMod";
+		public const string PLUGIN_VERSION = "1.0.0";
+
+		private void Awake()
+		{
+			// Plugin startup logic
+			Logger.LogInfo($"Plugin {PLUGIN_GUID} is loaded!");
+		}
+	}
+}
